a829d670e84dbf4e9f8a85a74ef2be5021253228
admodev <admodevcodes@outlook.com>
2022-09-26 23:23:10 -0300

    add multiple connections support

--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,24 @@ use tokio::{
 async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
 
-    let (mut socket, _address) = listener.accept().await.unwrap();
-
-    let (read, mut writer) = socket.split();
-
-    let mut reader = BufReader::new(read);
-
-    let mut line = String::new();
-
     loop {
-        let bytes_read = reader.read_line(&mut line).await.unwrap();
+        let (mut socket, _address) = listener.accept().await.unwrap();
 
-        if bytes_read == 0 {
-            break;
+        let (read, mut writer) = socket.split();
+
+        let mut reader = BufReader::new(read);
+
+        let mut line = String::new();
+
+        loop {
+            let bytes_read = reader.read_line(&mut line).await.unwrap();
+
+            if bytes_read == 0 {
+                break;
+            }
+
+            writer.write_all(&line.as_bytes()).await.unwrap();
+            line.clear();
         }
-
-        writer.write_all(&line.as_bytes()).await.unwrap();
-        line.clear();
     }
 }
